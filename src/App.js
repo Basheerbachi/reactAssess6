dc6bba3f0eb28ecf4887e863ca2c2efa149ed3ba
Basheerbachi <basheerbachi066@gmail.com>
2022-05-30 17:14:57 +0530

    new comments

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Contact from './components/contact';
+import Home from './components/home';
+import Students from './components/students';
+import Navbar from './components/navbar';
+import Edit from './components/edit'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/students' element={<Students />} />
+        <Route path='/student-desc' element={<Edit />} />
+        <Route path='/student-desc/:id' element={<Edit />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
+
+
